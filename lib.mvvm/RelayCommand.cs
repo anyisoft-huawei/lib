b96c4185c70d2df4b101145a7ee262b0be67b354
Me <yxhuawei@qq.com>
2021-04-25 13:19:19 +0800

    增加了CRC16和MVVM

---
+++ lib.mvvm/RelayCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace lib.mvvm
+{
+    class RelayCommand : ICommand
+    {
+        #region Fields
+        private readonly Func<object, bool> _canExecute;
+        private readonly Action<object> _execute;
+        #endregion
+
+        #region Constructors
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        #endregion
+
+        #region ICommand Members
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+        #endregion
+
+    }
+}
